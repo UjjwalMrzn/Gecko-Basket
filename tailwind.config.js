2d52485af51ed0f58de1d82cd86e4ccd7d5b46ee
ujjwalmrzn <ujjwalmrzn@gmail.com>
2025-06-26 09:36:44 +0545

    chore: update package dependencies and configurations
    - Bump version to 1.0.0 in package.json
    - Simplify build script in package.json
    - Update dependencies including MUI and React Router
    - Add PostCSS configuration for Tailwind CSS
    - Update Tailwind CSS configuration to center container and set padding
    - Modify TypeScript configurations for app and node
    - Update Vite configuration to optimize dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // <== Tailwind scans here
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
+ theme: {
+  container: {
+    center: true,
+    padding: "1rem",
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+    },
   },
+},
   plugins: [],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// remove tailwindcss here
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      tsconfigRaw: {}
+    }
+  }
 });
