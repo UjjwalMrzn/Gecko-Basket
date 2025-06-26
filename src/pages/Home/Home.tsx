856c4b242b86eaeaa9275e125f78a5ac60dc298f
ujjwalmrzn <ujjwalmrzn@gmail.com>
2025-06-26 10:33:02 +0545

    refactor: remove unused imports from Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,7 @@
 import Banner from "../../components/Banner/Banner";
 import Brand from "../../components/Brand/Brand";
 import Categories from "../../components/Categories/Categories";
-import Client from "../../components/Client/Client";
 import Product from "../../components/Product/Product";
-import Recent from "../../components/Recent/Recent";
 import Delivery from "../../components/Delivery/Delivery";
 import Features from "../../components/Features/Features";
 
